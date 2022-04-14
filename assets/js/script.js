9397cc5b181eaef33592866b7b57f613afa5bafc
Natalie Bulger <bulgern20@gmail.com>
2022-04-14 08:34:58 -0700

    Adding js file with onclick functions to projects

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(".card-column-1").on('click', function(){
+    window.open("https://nbulger1.github.io/my-recipes/","_blank");
+});
+
+$(".card-column-2").on('click', function(){
+    window.open("https://nbulger1.github.io/weather-dashboard/","_blank");
+});
+
+$(".card-column-3").on('click', function(){
+    window.open("https://nbulger1.github.io/code-quiz/","_blank");
+});
+
+$(".card-column-4").on('click', function(){
+    window.open("","_blank");
+});
+
+$(".card-column-5").on('click', function(){
+    window.open("","_blank");
+});
