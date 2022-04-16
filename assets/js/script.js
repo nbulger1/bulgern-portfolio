582235bbea6c73f0e79d2b618361bdce5cad011e
Natalie Bulger <bulgern20@gmail.com>
2022-04-16 12:37:53 -0700

    Adding links to github repos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Linking the card section to the deployed application
 $(".card-column-1").on('click', function(){
     window.open("https://nbulger1.github.io/my-recipes/","_blank");
 });
@@ -10,10 +11,15 @@ $(".card-column-3").on('click', function(){
     window.open("https://nbulger1.github.io/code-quiz/","_blank");
 });
 
-$(".card-column-4").on('click', function(){
-    window.open("","_blank");
+//Linking the repo text to the github repos
+$(".card-text-1").on('click', function(){
+    window.open("https://github.com/nbulger1/my-recipes.git","_blank");
 });
 
-$(".card-column-5").on('click', function(){
-    window.open("","_blank");
+$(".card-text-2").on('click', function(){
+    window.open("https://github.com/nbulger1/weather-dashboard.git","_blank");
+});
+
+$(".card-text-3").on('click', function(){
+    window.open("https://github.com/nbulger1/code-quiz.git","_blank");
 });
