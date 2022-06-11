9d5f912b844d1e8f23e0015606489104d3f968b6
Natalie Bulger <bulgern20@gmail.com>
2022-06-11 12:40:56 -0700

    Updating portfolio with bens diner and tech blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,41 @@
 //Linking the card section to the deployed application
-$(".card-column-1").on('click', function(){
-    window.open("https://nbulger1.github.io/my-recipes/","_blank");
+$(".card-column-1").on("click", function () {
+  window.open("https://bens-diner.herokuapp.com/", "_blank");
 });
 
-$(".card-column-2").on('click', function(){
-    window.open("https://nbulger1.github.io/weather-dashboard/","_blank");
+$(".card-column-2").on("click", function () {
+  window.open("https://nbulger1.github.io/my-recipes/", "_blank");
 });
 
-$(".card-column-3").on('click', function(){
-    window.open("https://nbulger1.github.io/code-quiz/","_blank");
+$(".card-column-3").on("click", function () {
+  window.open("https://tech-blog-bulger.herokuapp.com/", "_blank");
+});
+
+$(".card-column-4").on("click", function () {
+  window.open("https://nbulger1.github.io/code-quiz/", "_blank");
+});
+
+$(".card-column-5").on("click", function () {
+  window.open("https://nbulger1.github.io/weather-dashboard/", "_blank");
 });
 
 //Linking the repo text to the github repos
-$(".card-text-1").on('click', function(){
-    window.open("https://github.com/nbulger1/my-recipes.git","_blank");
+$(".card-text-1").on("click", function () {
+  window.open("https://github.com/wonjong2/Bens_Diner.git", "_blank");
 });
 
-$(".card-text-2").on('click', function(){
-    window.open("https://github.com/nbulger1/weather-dashboard.git","_blank");
+$(".card-text-2").on("click", function () {
+  window.open("https://github.com/nbulger1/my-recipes.git", "_blank");
 });
 
-$(".card-text-3").on('click', function(){
-    window.open("https://github.com/nbulger1/code-quiz.git","_blank");
+$(".card-text-3").on("click", function () {
+  window.open("https://github.com/nbulger1/tech-blog.git", "_blank");
+});
+
+$(".card-text-4").on("click", function () {
+  window.open("https://github.com/nbulger1/code-quiz.git", "_blank");
+});
+
+$(".card-text-5").on("click", function () {
+  window.open("https://github.com/nbulger1/weather-dashboard.git", "_blank");
 });
